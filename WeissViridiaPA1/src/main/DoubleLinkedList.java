e438cfe3c2f0a838337fcc942adfa1fe468d641f
Viridia Emmaline W <hjsthrowaway@gmail.com>
2022-10-10 00:09:04 -0400

    ya

--- WeissViridiaPA1/src/main/DoubleLinkedList.java
+++ WeissViridiaPA1/src/main/DoubleLinkedList.java
@@ -2,31 +2,90 @@ package main;
 
 public class DoubleLinkedList<T> {
 
+	private int size;
+	private Node<T> first;
+
+	/**
+	 * initializes a doubly linked list to have 0 elements.
+	 */
 	public DoubleLinkedList() {
-		
+		this.size = 0;
 	}
-	
+
+	/**
+	 * gets the first node in the list or null if one does not exist.
+	 */
 	public Node<T> getFirst() {
-		return null;
+		return this.first;
 	}
-	
-	
+
+	/**
+	 * adds an element to the end of this list.
+	 * 
+	 */
 	public void insert(T element) {
-		
+		if (this.first != null) {
+			Node<T> tempNode = this.first;
+			while (tempNode.getNext() != null) {
+				tempNode = tempNode.getNext();
+			}
+			tempNode.setNext(new Node<T>(element));
+			tempNode.getNext().setPrev(tempNode);
+		} else {
+			this.first = new Node<T>(element);
+		}
+		this.size++;
 	}
-	
+
+	/**
+	 * deletes the first element from this list that matches the
+	 * provided key. If the provided key does not exist in the list, return null.
+	 */
 	public T delete(T key) {
-		return null;
+		if (this.first != null) {
+
+			Node<T> tempNode = this.first;
+			while (tempNode.getNext().getData() != key) {
+
+				tempNode = tempNode.getNext();
+
+				if (tempNode.getNext() == null) {
+					if (tempNode.getData() == key) {
+						tempNode.getPrev().setNext(null);
+						this.size--;
+						return key;
+					}
+					return null;
+				}
+			}
+			tempNode.getPrev().setNext(tempNode.getNext());
+			tempNode.getNext().setPrev(tempNode.getPrev());
+			this.size--;
+			return key;
+
+		} else {
+			return null;
+		}
 	}
-	
+
+	/**
+	 * returns the first element in the list that matches the provided key
+	 * or null if one cannot be found.
+	 * 
+	 * 
+	 */
 	public T get(T key) {
 		return null;
 	}
-	
+
+	/**
+	 * @return returns the number of elements in the list.
+	 * 
+	 */
 	public int size() {
-		return -1;
+		return this.size;
 	}
-	
+
 	@Override
 	public String toString() {
 		return null;
--- WeissViridiaPA1/src/main/Node.java
+++ WeissViridiaPA1/src/main/Node.java
@@ -1,37 +1,65 @@
 package main;
 
 public class Node<T> {
-	
+
+	private T data;
+	private Node<T> next;
+	private Node<T> prev;
+
+	/**
+	 * Constructs a doubly linked list node that holds a data field but
+	 * does not point to any other nodes.
+	 */
 	public Node(T data) {
-		
+		this.data = data;
 	}
-	
+
+	/**
+	 * Sets the data field of this node.
+	 */
 	public void setData(T data) {
-		
+		this.data = data;
 	}
-	
+
+	/**
+	 * Sets the next pointer of this node.
+	 */
 	public void setNext(Node<T> next) {
-		
+		this.next = next;
 	}
-	
+
+	/**
+	 * Sets the previous pointer of this node.
+	 */
 	public void setPrev(Node<T> prev) {
-		
+		this.prev = prev;
 	}
-	
+
+	/**
+	 * @return returns the pointer to the next node or null if one does
+	 *         not exist.
+	 */
 	public Node<T> getNext() {
-		return null;
+		return this.next;
 	}
-	
+
+	/**
+	 * @return returns the pointer to the previous node or null if one
+	 *         does not exist.
+	 */
 	public Node<T> getPrev() {
-		return null;
+		return this.prev;
 	}
-	
+
+	/**
+	 * @return the data stored in this node.
+	 */
 	public T getData() {
-		return null;
+		return this.data;
 	}
-	
+
 	@Override
 	public String toString() {
-		return null;
+		return this.data.toString();
 	}
 }
