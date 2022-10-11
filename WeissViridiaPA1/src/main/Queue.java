8f6ae37cc9f4ba52cd1350e29ecdfb2f43dafc51
Viridia Emmaline W <hjsthrowaway@gmail.com>
2022-10-10 20:09:49 -0400

    up

--- WeissViridiaPA1/src/main/Queue.java
+++ WeissViridiaPA1/src/main/Queue.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.util.NoSuchElementException;
+
 public class Queue<T> {
 
 	/**
@@ -21,27 +23,73 @@ public class Queue<T> {
 	 */
 	public int numEntries;
 
+	/**
+	 * constructs an empty queue that can hold a specified
+	 * number of elements.
+	 * 
+	 * @param capacity
+	 */
 	@SuppressWarnings("unchecked")
 	public Queue(int capacity) {
 		this.q = (T[]) new Object[capacity];
+		this.numEntries = 0;
+		this.head = 0;
+		this.tail = 0;
 	}
 
+	/**
+	 * adds an element at the tail of the queue.
+	 */
 	public void enqueue(T element) {
-
+		if (this.numEntries == this.q.length) {
+			throw new IndexOutOfBoundsException();
+		} else {
+			this.q[this.tail] = element;
+			this.numEntries++;
+			this.tail++;
+			this.tail = this.tail % this.q.length;
+		}
 	}
 
+	/**
+	 * removes the element at the head of the queue. If there is no
+	 * such element, <strong>you must throw a<strong>
+	 * <code>NoSuchElementException</code>.
+	 */
 	public void dequeue() {
+		if (this.tail == this.head) {
+			throw new NoSuchElementException();
+		} else {
+			this.q[this.head] = null;
+			this.head++;
+			this.numEntries--;
+			this.head = this.head % this.q.length;
+		}
 
 	}
 
+	/**
+	 * returns the element at the head of the queue. If there is no
+	 * such element, <strong>you must throw a<strong>
+	 * <code>NoSuchElementException</code>.
+	 */
 	public T front() {
 		return null;
 	}
 
+	/**
+	 * return the number of elements in the queue.
+	 */
 	public int size() {
 		return -1;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 * return a String representation of the queue’s elements.
+	 */
 	@Override
 	public String toString() {
 		return null;
