67838d5c3af6bf8f9b656d254863d8ab0c1e5857
Viridia Emmaline <viridiaemmaline@gmail.com>
2022-10-13 02:31:06 -0400

    yayay

--- WeissViridiaPA1/src/main/Station.java
+++ WeissViridiaPA1/src/main/Station.java
@@ -1,3 +1,13 @@
+/** 
+  * Station implimentation
+  * Known Bugs: None
+  * 
+  * @author Viridia Weiss   
+  * @email gweiss@brandeis.edu
+  * October 9th, 2022
+  * COSI 21A PA1  
+  */
+
 package main;
 
 public class Station {
@@ -21,38 +31,42 @@ public class Station {
 	public Queue<Train> southBoundTrains;
 	private String name;
 
+	public static final int QUEUE_CAPACITY = 20;
+
 	/**
 	 * constructs an empty Station with a given name.
+	 * O(1)
 	 * 
 	 * @param name
 	 */
 	public Station(String name) {
 		this.name = name;
-		this.northBoundRiders = new Queue<Rider>(0);
-		this.southBoundRiders = new Queue<Rider>(0);
-		this.northBoundTrains = new Queue<Train>(0);
-		this.southBoundTrains = new Queue<Train>(0);
+		this.northBoundRiders = new Queue<Rider>(QUEUE_CAPACITY);
+		this.southBoundRiders = new Queue<Rider>(QUEUE_CAPACITY);
+		this.northBoundTrains = new Queue<Train>(QUEUE_CAPACITY);
+		this.southBoundTrains = new Queue<Train>(QUEUE_CAPACITY);
 	}
 
 	/**
 	 * adds a Rider to the appropriate Queue,
 	 * depending on the Rider’s direction, as long as they should be in this
 	 * Station. Return true if this is
-	 * possible and false otherwise.
+	 * possible and false otherwise. O(1)
 	 * 
 	 * @param r
 	 * @return
 	 */
 	public boolean addRider(Rider r) {
 		if (r.getStarting() == this.name) {
-			if (r.goingNorth()) {
+			if (r.goingNorth() && this.northBoundRiders.size() > 0) {
 				this.northBoundRiders.enqueue(r);
-			} else {
+			} else if (!r.goingNorth() && this.southBoundRiders.size() > 0) {
 				this.southBoundRiders.enqueue(r);
 			}
 			return true;
 		}
 		return false;
+
 	}
 
 	/**
@@ -64,15 +78,15 @@ public class Station {
 	 * that some passengers were removed at this Station. For instance, one could
 	 * return:
 	 * Quincy Adams Disembarking Passengers:
-	 * FVPRE0BX09L3OMS2VR87
+	 * FVPRE0BX09L3OMS2VR87 O(n)
 	 * 
 	 * @param t
 	 * @return
 	 */
 	public String addTrain(Train t) {
-		if (t.goingNorth()) {
+		if (t.goingNorth() && this.northBoundTrains.size() > 0) {
 			this.northBoundTrains.enqueue(t);
-		} else {
+		} else if (!t.goingNorth() && this.southBoundTrains.size() > 0) {
 			this.southBoundTrains.enqueue(t);
 		}
 		return this.name + " Disembarking Passengers:\n" + t.disembarkPassengers();
@@ -85,7 +99,7 @@ public class Station {
 	 * 2) Adding as many southbound Riders to the Train as possible.
 	 * 3) Return the train that has been filled or null if there are no waiting
 	 * southbound
-	 * Trains.
+	 * Trains. O(n)
 	 * 
 	 * @return
 	 */
@@ -110,7 +124,7 @@ public class Station {
 	 * 2) Adding as many northbound Riders to the Train as possible.
 	 * 3) Return the train that has been filled or null if there are no waiting
 	 * northbound
-	 * Trains.
+	 * Trains. O(n)
 	 * 
 	 * @return
 	 */
@@ -132,26 +146,32 @@ public class Station {
 	 * changes the direction of the first waiting
 	 * northbound Train and moves it to the southbound queue. You may assume that
 	 * the Train will not
-	 * have any Riders.
+	 * have any Riders. O(1)
 	 * 
 	 */
 	public void moveTrainNorthToSouth() {
-		Train deqTrain = this.northBoundTrains.front();
-		this.northBoundTrains.dequeue();
-		this.southBoundTrains.enqueue(deqTrain);
+		if (this.southBoundTrains.size() < QUEUE_CAPACITY && this.northBoundTrains.size() > 0) {
+			Train deqTrain = this.northBoundTrains.front();
+			deqTrain.swapDirection();
+			this.northBoundTrains.dequeue();
+			this.southBoundTrains.enqueue(deqTrain);
+		}
 	}
 
 	/**
 	 * changes the direction of the first waiting
 	 * southbound Train and moves it to the northbound queue. You may assume that
 	 * the Train will not
-	 * have any Riders.
+	 * have any Riders. O(1)
 	 * 
 	 */
 	public void moveTrainSouthToNorth() {
-		Train deqTrain = this.southBoundTrains.front();
-		this.southBoundTrains.dequeue();
-		this.northBoundTrains.enqueue(deqTrain);
+		if (this.northBoundTrains.size() < QUEUE_CAPACITY && this.southBoundTrains.size() > 0) {
+			Train deqTrain = this.southBoundTrains.front();
+			deqTrain.swapDirection();
+			this.southBoundTrains.dequeue();
+			this.northBoundTrains.enqueue(deqTrain);
+		}
 	}
 
 	/**
@@ -161,7 +181,7 @@ public class Station {
 	 * 1 north-bound trains waiting
 	 * 0 south-bound trains waiting
 	 * 0 north-bound passengers waiting
-	 * 1 south-bound passengers waiting
+	 * 1 south-bound passengers waiting O(1)
 	 * 
 	 * @see java.lang.Object#toString()
 	 */
@@ -174,22 +194,27 @@ public class Station {
 	}
 
 	/**
-	 * returns the name of this Station
+	 * returns the name of this Station O(1)
 	 * 
 	 * @return
 	 */
 	public String stationName() {
-		return null;
+		return this.name;
 	}
 
 	/**
 	 * Checks if a Station is equal to some object
-	 * based on name.
+	 * based on name. O(1)
 	 * 
 	 * @see java.lang.Object#equals(java.lang.Object)
 	 */
 	@Override
 	public boolean equals(Object o) {
-		return false;
+		if (!(o instanceof Station)) {
+			return false;
+		}
+		Station r = (Station) o;
+		return (this.name == r.stationName());
+
 	}
 }
